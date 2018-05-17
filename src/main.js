4970e92c98aa4286a08bd1abf4758a08e7b8302b
helianyang <471177435@qq.com>
2018-05-17 13:10:21 +0800

    更改列表渲染方式，通过列表数组实时渲染，增加删除功能，因为列表是实时渲染，所以列表的索引号和数组的索引号是一致的通过 Array.prototype.indexOf.call(this.parentNode.childNodes, this)方法所点击列表的索引，通过索引删除对用的数组元素，从而完成删除功能

--- src/main.js
+++ src/main.js
@@ -19,40 +19,68 @@
   function additem() {
     let btn = document.getElementsByClassName('btn')[0];
     let addList = document.getElementById('add-list');
-    
+    let todoitem = {};
     addEvent(btn, 'click', function () {
       if (!addList.value) {
         return;
       }
-      todolist.push(addList.value);
+      todoitem = {
+        time: getNowTime(),
+        task: addList.value,
+      };
+      todolist.push(todoitem);
       console.log(todolist);
       addList.value = '';
       listShow();
     })
 
   }
+  //显示在列表中
   function listShow() {
-    let li = document.createElement('li');
-    let p = document.createElement('p');
-    let list = document.getElementsByClassName('todolist')[0];
-    todolist.forEach(ele => {
-      p.innerHTML = '<span></span>' + ele;
-      list.appendChild(li).appendChild(p)
+    let list = document.getElementsByClassName('todolist')[0];   
+    list.innerHTML = '';
+    todolist.forEach((ele, index) => {
+      let li = document.createElement('li');
+      let p = document.createElement('p');
+      let em = document.createElement('em');
+      p.innerHTML = '<span></span><i>' + ele.time + '</i>' + ele.task;
+      let item = list.appendChild(li);
+      item.appendChild(p);
+      item.appendChild(em);
     });
   }
+  //完成任务
   function getItem() {
     let list = document.getElementsByClassName('todolist')[0];
     addEvent(list, 'click', function (e) {
       //兼容性处理
       e = e || event;
       let target = e.target || ev.srcElement;
+     
       if (target.nodeName === 'P') {
         target.style.textDecoration = 'line-through';
         target.childNodes[0].style.background = 'url(../images/success.png) no-repeat';
         target.childNodes[0].style.backgroundSize = '100%';
-        console.dir(target.childNodes[0]);
       }
-    })
+      if (target.nodeName === 'EM') {
+        //删除一条待办事项
+        let liItem = target.parentNode;
+        let index = Array.prototype.indexOf.call(liItem.parentNode.childNodes, liItem);
+        todolist.splice(index, 1);
+        listShow();
+      }
+    });
+  }
+  
+  function getNowTime() {
+    let foo = new Date();
+    let now = foo.getMonth() + 1 + '月' + foo.getDate() + '日' + foo.getHours() + ':' + foo.getMinutes();
+    return now;
+  }
+
+  function removeItem(index) {
+    todolist.splice(index, 1);
+    
   }
   additem();
   getItem()
